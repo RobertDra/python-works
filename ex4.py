d0183a93e36ecdccc8523639284caaac6a51990f
Robert Drannikov <bittzzd@gmail.com>
2020-07-17 20:46:40 +0300

    Third Task

--- ex4.py
+++ ex4.py
@@ -1,8 +1,15 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-newlist = []
-num2 = int(input('enter a number'))
-for num in a:
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+new_list = []
 
-    if num < num2:
-        newlist.append(num)
-print(newlist)
+def print_all_numbers_less_then_chosen_number():
+    number_chosen = int(input('enter a number'))
+    for number in my_list:
+        if number < number_chosen:
+            new_list.append(number)
+    print(new_list)
+
+def main():
+    print_all_numbers_less_then_chosen_number()
+
+if __name__ == '__main__':
+    main()
