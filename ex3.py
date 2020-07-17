3b1f2189b37333f458c2db042a6fbbc193ed063a
Robert Drannikov <bittzzd@gmail.com>
2020-07-17 20:46:00 +0300

    Seconf Task

--- ex3.py
+++ ex3.py
@@ -1,5 +1,12 @@
-num = int(input('enter a number'))
-if (num % 2) == 0:
-   print((num) , 'is Even')
-else:
-   print((num) , 'is Odd')
+def even_or_odd():
+   number_to_choose = int(input('enter a number'))
+   if (number_to_choose % 2) == 0:
+      print((number_to_choose), 'is Even')
+   else:
+      print((number_to_choose), 'is Odd')
+
+def main():
+   even_or_odd()
+
+if __name__ == '__main__':
+   main()
