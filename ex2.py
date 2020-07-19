e783da30668a13d0a51fc931d72bd76a85043877
Robert Drannikov <bittzzd@gmail.com>
2020-07-19 20:27:41 +0300

    First Task

--- ex2.py
+++ ex2.py
@@ -1,18 +1,18 @@
-def calculate_the_amount_of_years_till_100(y, z):
-    return y-z+100
-
-def enter_information():
+def name_and_years_information():
     name = input('Enter your name:')
     year = int(input('what year it is?'))
     years_old = int(input('how old are u?'))
 
-def print():
-    print('you will be 100 years old at', calculate_the_amount_of_years_till_100())
+def calculate_the_amount_of_years_till_100(year, years_old):
+    return year-years_old+100
+
+def the_result():
+    print('you will be 100 years old at',calculate_the_amount_of_years_till_100(year, years_old))
 
 def main():
-    enter_information()
+    name_and_years_information()
     calculate_the_amount_of_years_till_100(year, years_old)
-    print()
+    the_result()
 
 if __name__ == '__main__':
     main()
