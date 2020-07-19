5228aea1dd617158d882790166bc7699d40d0f4d
Robert Drannikov <bittzzd@gmail.com>
2020-07-19 20:28:20 +0300

    Second Task

--- ex3.py
+++ ex3.py
@@ -1,12 +1,20 @@
-def even_or_odd():
+def get_number_from_user():
    number_to_choose = int(input('enter a number'))
-   if (number_to_choose % 2) == 0:
-      print((number_to_choose), 'is Even')
+
+def even_or_odd(given_number):
+
+   if (given_number % 2) == 0:
+     return ('number is even')
    else:
-      print((number_to_choose), 'is Odd')
+     return ('number is odd')
+
+def output_message(state):
+  print(state)
 
 def main():
-   even_or_odd()
+   given_number = get_number_from_user()
+   state = even_or_odd(given_number)
+   output_message(state)
 
 if __name__ == '__main__':
    main()
