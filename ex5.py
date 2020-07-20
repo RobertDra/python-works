e4b13747e6ba034d4015482c415ac66b083bb4d1
Robert Drannikov <bittzzd@gmail.com>
2020-07-20 15:25:09 +0300

    Fourth Task

--- ex5.py
+++ ex5.py
@@ -1,6 +1,22 @@
-number_to_choose=int(input('please eenter a number'))
 list_of_divisors=[]
-for divisors in range(1,number_to_choose+1):
- if number_to_choose%divisors==0:
-    list_of_divisors.append(divisors)
-print(list_of_divisors)
+
+def chosen_number():
+    number_to_choose = int(input('please enter a number'))
+    return number_to_choose
+
+def divisor(chosen):
+    for divisors in range(1, chosen + 1):
+        if chosen % divisors == 0:
+            list_of_divisors.append(divisors)
+
+def result():
+    print(list_of_divisors)
+
+def main():
+    chosen = chosen_number()
+    divisor(chosen)
+    result()
+
+
+if __name__ == '__main__':
+    main()
