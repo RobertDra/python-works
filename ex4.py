e9f6d92f0b3cee809e283b92b209e77a552d554c
Robert Drannikov <bittzzd@gmail.com>
2020-07-20 15:24:34 +0300

    Third Task

--- ex4.py
+++ ex4.py
@@ -3,18 +3,19 @@ new_list = []
 
 def chosen_number():
     number_chosen = int(input('enter a number'))
+    return number_chosen
 
-def check_in_list(less_then_the_number):
+def check_in_list(less_than_the_number):
     for number in my_list:
-        if number < less_then_the_number:
+        if number < less_than_the_number:
             new_list.append(number)
 
 def output_message():
     print(new_list)
 
 def main():
-    less_then_the_number = chosen_number()
-    check_in_list(less_then_the_number)
+    less_than_the_number = chosen_number()
+    check_in_list(less_than_the_number)
     output_message()
 
 if __name__ == '__main__':
